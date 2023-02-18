1b8f3f3897cd24d3d792bc67be0e55805938153d
Matt Gabrenya <matt@buildyourweb.app>
2023-02-17 21:28:12 -0700

    fix: compare integrity zome wasm builds, not dnas, for clarity

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,13 @@
 cd example1
 CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown
 hc dna pack dnas/forum/workdir
+cd ../
 
-cd ../example2
+cd example2
 CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown
 hc dna pack dnas/forum/workdir
 cd ../
 
 echo ""
-echo "forum dna md5sum: $(md5sum example1/dnas/forum/workdir/forum.dna)"
-echo "forum2 dna md5sum: $(md5sum example2/dnas/forum/workdir/forum.dna)"
+echo "forum dna md5sum: $(md5sum example1/target/wasm32-unknown-unknown/release/posts_integrity.wasm)"
+echo "forum dna md5sum: $(md5sum example2/target/wasm32-unknown-unknown/release/posts_integrity.wasm)"
