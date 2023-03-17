75096bdf26b521b16dde27ae3bdab4fc3d3cdcc7
Matt Gabrenya <matt@buildyourweb.app>
2023-03-17 09:33:25 -0700

    test: show that converting to wat and back resolves hash inconsistency

--- run-tests.sh
+++ run-tests.sh
@@ -11,3 +11,19 @@ cd ../
 echo ""
 echo "example1 posts_integrity zome md5sum: $(md5sum example1/target/wasm32-unknown-unknown/release/posts_integrity.wasm)"
 echo "example2 posts_integrity zome md5sum: $(md5sum example2/target/wasm32-unknown-unknown/release/posts_integrity.wasm)"
+
+
+echo "example1 posts_integrity zome md5sum: $(md5sum example1/target/wasm32-unknown-unknown/release/posts_integrity.wasm)"
+echo "example2 posts_integrity zome md5sum: $(md5sum example2/target/wasm32-unknown-unknown/release/posts_integrity.wasm)"
+
+wasm2wat example1/target/wasm32-unknown-unknown/release/posts_integrity.wasm > example1.wat
+wasm2wat example2/target/wasm32-unknown-unknown/release/posts_integrity.wasm > example2.wat
+
+echo "example1 wasm2wat output md5sum: $(md5sum example1.wat)"
+echo "example2 wasm2wat output md5sum: $(md5sum example2.wat)"
+
+wat2wasm example1/target/wasm32-unknown-unknown/release/posts_integrity.wasm > example1.wasm
+wat2wasm example2/target/wasm32-unknown-unknown/release/posts_integrity.wasm > example2.wasm
+
+echo "example1 wat2wasm output md5sum: $(md5sum example1.wasm)"
+echo "example2 wat2wasm output md5sum: $(md5sum example2.wasm)"
